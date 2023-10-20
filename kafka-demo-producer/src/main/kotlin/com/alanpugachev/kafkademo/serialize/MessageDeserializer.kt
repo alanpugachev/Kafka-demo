9a398fc94ac67f737f250836e27ef688d86bea4b
alanpugachev <aszneesizzle@gmail.com>
2023-10-20 17:12:02 +0300

    Add deserializer

--- kafka-demo-producer/src/main/kotlin/com/alanpugachev/kafkademo/serialize/MessageDeserializer.kt
+++ kafka-demo-producer/src/main/kotlin/com/alanpugachev/kafkademo/serialize/MessageDeserializer.kt
@@ -3,26 +3,26 @@ package com.alanpugachev.kafkademo.serialize
 import com.alanpugachev.kafkademo.dto.MessageDto
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.kafka.common.errors.SerializationException
-import org.apache.kafka.common.serialization.Serializer
+import org.apache.kafka.common.serialization.Deserializer
 
-class MessageSerializer: Serializer<MessageDto> {
+class MessageDeserializer: Deserializer<MessageDto> {
     private val objectMapper = ObjectMapper()
 
     override fun configure(configs: MutableMap<String, *>?, isKey: Boolean) {
         super.configure(configs, isKey)
     }
 
-    override fun serialize(topic: String?, data: MessageDto?): ByteArray? {
+    override fun deserialize(topic: String?, data: ByteArray?): MessageDto? {
         try {
             if (data == null) {
-                println("Null received at serializing")
+                println("Null received at deserializing")
                 return null
             }
 
-            println("Serializing...")
-            return objectMapper.writeValueAsBytes(data)
+            println("Deserializing...")
+            return objectMapper.readValue(String(data), MessageDto::class.java)
         } catch (e: Exception) {
-            throw SerializationException("Error when serializing MessageDto")
+            throw SerializationException("Error when deserializing MessageDto")
         }
     }
 }
